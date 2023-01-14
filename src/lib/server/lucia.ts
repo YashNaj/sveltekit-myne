447cd49cec6c7730eb9b06cf0e28b7ecfce048b3
Yashal Najeeb <Yash@YashaLs-MBP.askey.com>
2023-01-14 12:06:19 -0800

    mongoose

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -2,7 +2,46 @@ import { dev } from '$app/environment';
 import lucia, { type UserData } from 'lucia-auth';
 import adapter from '@lucia-auth/adapter-mongoose';
 import mongoose from 'mongoose';
-
+const User = mongoose.model(
+    "user",
+    new mongoose.Schema(
+        {
+            _id: {
+                type: String
+            },
+            provider_id: {
+                type: String,
+                unique: true,
+                required: true
+            },
+            hashed_password: String
+        },
+        { _id: false }
+    )
+);
+const Session = mongoose.model(
+    "session",
+    new mongoose.Schema(
+        {
+            _id: {
+                type: String
+            },
+            user_id: {
+                type: String,
+                required: true
+            },
+            expires: {
+                type: Number,
+                required: true
+            },
+            idle_expires: {
+                type: Number,
+                required: true
+            }
+        },
+        { _id: false }
+    )
+);
 export const auth = lucia({
 	adapter: adapter(mongoose),
 	env: dev ? 'DEV' : 'PROD',
