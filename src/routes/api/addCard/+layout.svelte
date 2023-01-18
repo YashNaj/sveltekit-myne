8026489ba0338ec2026df90dd280ab30a8521c4a
YashNaj <yashalnajeeb@gmail.com>
2023-01-17 22:07:47 -0800

    profile page updates

---
+++ src/routes/api/addCard/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang = 'ts'>
+	import { horizontalSlide } from '$lib/animationActions';
+	import MobileNav from '$lib/components/MobileNav.svelte';
+	import { blur, fly, slide } from 'svelte/transition';
+</script>
+
+<div
+	transition:horizontalSlide={{ axis: 'x', duration: 400, delay: 400 }}
+	class="container h-screen w-screen"
+>
+	<slot class="w-screen h-80 m-2" />
+</div>
---
+++ src/routes/api/addCard/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { fly, slide } from 'svelte/transition';
+	let duration = 200;
+</script>
+
+<div
+transition:slide={{duration: 200, delay: 200}}
+	class="container h-auto w-full flex-col add-cards"
+>
+	<div class = 'container h-auto w-full p-2 bg-white'>
+
+	</div>
+</div>
